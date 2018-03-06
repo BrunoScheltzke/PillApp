fd1a1e6d512c081f2371a13c3b9ce69b247eeda2
Arthur Giachini <arthur.giachini@acad.pucrs.br>
2018-03-06 16:16:11 -0300

    flow frame

---
+++ PillApp/CreateMedicineViewController.swift
@@ -0,0 +1,37 @@
+//
+//  CreateMedicineViewController.swift
+//  PillApp
+//
+//  Created by Arthur Giachini on 06/03/2018.
+//  Copyright © 2018 Bruno Scheltzke. All rights reserved.
+//
+
+import UIKit
+
+class CreateMedicineViewController: UIViewController {
+    
+    @IBOutlet weak var medicineNameTxt: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- PillApp/ViewController.swift
+++ PillApp/ViewController.swift
@@ -10,8 +10,73 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var noMedicineImage: UIImageView!
+    
+    var medicines = [Medicine]()
+    var daysOfWeek = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        //medicines.append(Medicine(name: "Paracetamol", quantity: 9, brand: "", unit: 9))
+        initTableView()
+        verifyTableViewContent()
     }
+    
+    func verifyTableViewContent() {
+        if medicines.count == 0 {
+            let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(callPerform(tapGestureRecognizer:)))
+            noMedicineImage.isUserInteractionEnabled = true
+            noMedicineImage.addGestureRecognizer(tapGestureRecognizer)
+            
+            noMedicineImage.isHidden = false
+            tableView.isHidden = true
+            
+        } else {
+            noMedicineImage.isHidden = true
+            tableView.isHidden = false
+        }
+    }
+    
+    @objc func callPerform(tapGestureRecognizer: UITapGestureRecognizer) {
+        performSegue(withIdentifier: "createMedicine", sender: nil)
+    }
+    
+}
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func initTableView() {
+        tableView.dataSource = self
+        tableView.delegate = self
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return daysOfWeek.count
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return medicines.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "medicineCell", for: indexPath) as! MedicineTableViewCell
+        
+        cell.medicine = medicines[indexPath.row]
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == UITableViewCellEditingStyle.delete {
+            medicines.remove(at: indexPath.row)
+            tableView.reloadData()
+        }
+    }
+    
 }
 
