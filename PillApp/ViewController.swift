95cedb305c0daa21aa801d1412d45b70899a0bdd
Bruno Scheltzke <brunofonten@gmail.com>
2018-03-06 12:49:08 -0300

    project setup

---
+++ PillApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PillApp
+//
+//  Created by Bruno Scheltzke on 06/03/18.
+//  Copyright © 2018 Bruno Scheltzke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
