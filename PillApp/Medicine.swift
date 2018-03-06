a86a6ae0d08310e039f9bba65795e6e63ff12114
Bruno Scheltzke <brunofonten@gmail.com>
2018-03-06 13:50:48 -0300

    added Medicine struct

---
+++ PillApp/Medicine.swift
@@ -0,0 +1,16 @@
+//
+//  Pill.swift
+//  PillApp
+//
+//  Created by Bruno Scheltzke on 06/03/18.
+//  Copyright © 2018 Bruno Scheltzke. All rights reserved.
+//
+
+import Foundation
+
+struct Medicine {
+    var name: String
+    var quantity: Int
+    var brand: String?
+    var unit: Int?
+}
