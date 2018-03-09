0412a47c05b3e5ab7f034a302e46d9295604ed18
Bruno Scheltzke <brunofonten@gmail.com>
2018-03-09 08:53:03 -0300

    bugfix for fetch today reminders as it was not fetching the reminders of concurrences

--- PillApp/CoreDataManager.swift
+++ PillApp/CoreDataManager.swift
@@ -97,8 +97,10 @@ class CoreDataManager {
         let dateTo = calendar.date(from: components)! // eg. 2016-10-11 00:00:00
         // Note: Times are printed in UTC. Depending on where you live it won't print 00:00:00 but it will work with UTC times which can be converted to local time
         
+        let currentWeekDay = calendar.component(.weekday, from: Date())
+        
         // Set predicate as date being today's date
-        let datePredicate = NSPredicate(format: "(%@ <= date) AND (date < %@)", argumentArray: [dateFrom, dateTo])
+        let datePredicate = NSPredicate(format: "((%@ <= date) AND (date < %@)) OR (frequency == %@) OR (frequency == %@)", argumentArray: [dateFrom, dateTo, Int32(currentWeekDay), Int32(Frequency.everyDay.rawValue)])
         request.predicate = datePredicate
         
         var reminders: [Reminder]?
@@ -110,9 +112,27 @@ class CoreDataManager {
         }
         
         let fetchRequest = NSFetchRequest<Register>(entityName: Keys.Register.tableName)
-        fetchRequest.predicate = NSPredicate(format: "(%@ <= date) AND (date < %@) AND (\(Keys.Register.taken) == %@", argumentArray: [dateFrom, dateTo, true])
+        fetchRequest.predicate = NSPredicate(format: "(%@ <= date) AND (date < %@) AND (\(Keys.Register.taken) == %@)", argumentArray: [dateFrom, dateTo, true])
+        var todayRegisters: [Register]?
         
-        return reminders
+        do {
+            todayRegisters = try container.viewContext.fetch(fetchRequest)
+        } catch {
+            print(error)
+        }
+        
+        let remindersNotChecked = reminders?.filter({ (reminder) -> Bool in
+            var wasReminderNotChecked = true
+            todayRegisters?.forEach({ (register) in
+                if reminder.objectID == register.reminder?.objectID{
+                    wasReminderNotChecked = false
+                    return
+                }
+            })
+            return wasReminderNotChecked
+        })
+        
+        return remindersNotChecked
     }
     
     func fetchAllMedicines() -> [Medicine]? {
@@ -151,7 +171,10 @@ class CoreDataManager {
     
     func createMockData() {
         let paracetamol = createMedicine(name: "Paracetamol", brand: "Agafarma", unit: 50, dosage: .pill)
-        createReminder(date: Date(), dosage: .pill, frequency: .everyDay, quantity: 1, medicine: paracetamol)
+        let diclofenaco = createMedicine(name: "Diclofenaco", brand: nil, unit: 30, dosage: .pill)
+        let paracetamolReminder = createReminder(date: Date(), dosage: .pill, frequency: .everyDay, quantity: 1, medicine: paracetamol)
+        createReminder(date: Date(), dosage: .pill, frequency: .currentDayOnly, quantity: 1, medicine: diclofenaco)
+        createRegister(date: Date(), reminder: paracetamolReminder, taken: true)
     }
     
     func toDictionary(_ reminder: Reminder) -> [String: Any] {
