55b0ebe1db785a6d8abf8a4bda9a3d3485db256e
Homero Oliveira <homero.santos.001@acad.pucrs.br>
2018-04-10 02:28:00 -0300

    done

--- PillApp/StepOneViewController.swift
+++ PillApp/StepOneViewController.swift
@@ -9,7 +9,12 @@
 import UIKit
 
 class StepOneViewController: UIViewController {
-
+    
+    @IBOutlet weak var topConstraintPillImage: NSLayoutConstraint!
+    @IBOutlet weak var topConstraintText: NSLayoutConstraint!
+    @IBOutlet weak var heightPillImage: NSLayoutConstraint!
+    @IBOutlet weak var widthPillImage: NSLayoutConstraint!
+    @IBOutlet weak var bottomConstraintNextButton: NSLayoutConstraint!
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -19,7 +24,54 @@ class StepOneViewController: UIViewController {
         tranparentNavigationBar()
         UIApplication.shared.statusBarStyle = .lightContent
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        setUpKeyboardObservers()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        removeKeyboardObservers()
+    }
+    
+    private func setUpKeyboardObservers() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: NSNotification.Name.UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: NSNotification.Name.UIKeyboardWillHide, object: nil)
+    }
+    
+    private func removeKeyboardObservers() {
+        NotificationCenter.default.removeObserver(self)
+    }
+    
     @IBAction func cancelButtonPressed(_ sender: UIBarButtonItem) {
         dismiss(animated: true, completion: nil)
+    }
+    
+    @objc func keyboardWillShow(notification: NSNotification) {
+        guard let keyboardFrame: NSValue = notification.userInfo?[UIKeyboardFrameEndUserInfoKey] as? NSValue else { return }
+        let keyboardRectangle = keyboardFrame.cgRectValue
+        let keyboardHeight = keyboardRectangle.height
+        view.layoutIfNeeded()
+        UIView.animate(withDuration: 0.5) {
+           self.topConstraintPillImage.constant = 0
+            self.heightPillImage.constant = 0
+            self.widthPillImage.constant = 0
+            self.topConstraintText.constant = 0
+            self.bottomConstraintNextButton.constant = keyboardHeight
+            self.view.layoutIfNeeded()
+        }
+    }
+    
+    @objc func keyboardWillHide(notification: NSNotification) {
+        view.layoutIfNeeded()
+        UIView.animate(withDuration: 0.5) {
+            self.topConstraintPillImage.constant = 44
+            self.heightPillImage.constant = 145
+            self.widthPillImage.constant = 135
+            self.topConstraintText.constant = 50
+            self.bottomConstraintNextButton.constant = 36
+            self.view.layoutIfNeeded()
+        }
     }
 }
